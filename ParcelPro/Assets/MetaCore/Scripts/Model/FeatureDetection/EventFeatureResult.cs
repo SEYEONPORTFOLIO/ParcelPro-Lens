c5fb575a040c547bcc4e28ff6dfa1fb3eb35bc38
LEE SE YEON <121601875+SEYEONPORTFOLIO@users.noreply.github.com>
2023-10-20 01:21:06 +0900

    씬 생성 및 스크립트 주석 추가
    Three Boxes : 3개 박스 띄울 씬
    Script->Model->FeatureDetection 스크립트 추석처리

--- ParcelPro/Assets/MetaCore/Scripts/Model/FeatureDetection/EventFeatureResult.cs
+++ ParcelPro/Assets/MetaCore/Scripts/Model/FeatureDetection/EventFeatureResult.cs
@@ -1,7 +1,9 @@
+// 필요한 네임스페이스를 사용합니다.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// JSON 직렬화를 위해 Newtonsoft.Json 라이브러리를 사용합니다.
 using Newtonsoft.Json;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -12,25 +14,31 @@ namespace Pnc
     {
         namespace FeatureDetection
         {
+            // [System.Serializable] 어트리뷰트를 사용하여 클래스를 직렬화 가능하게 만듭니다.
             [System.Serializable]
-            public class EventFeatureResult : PncEvent
+            public class EventFeatureResult : PncEvent //[클래스 설명]  PncEvent를 확장하고 특징 검출 결과를 포함하는 이벤트 객체 클래스.
             {
+                // FeatureResult 객체를 포함하는 속성을 정의합니다.
                 public FeatureResult featureResult;
 
+                // FeatureResult 객체를 반환하는 메서드를 정의합니다.
                 public FeatureResult getFeatureResult()
                 {
                     return featureResult;
                 }
 
+                // FeatureResult 객체를 설정하는 메서드를 정의합니다.
                 public void setFeatureResult(FeatureResult featureresult)
                 {
                     this.featureResult = featureresult;
                 }
 
+                // 객체 정보를 문자열로 반환하는 메서드를 정의합니다.
                 public string info()
                 {
                     string buff = string.Empty;
 
+                    // 객체 정보를 문자열로 구성합니다.
                     buff = $"EventFeatureResult=[owner={getOwner()}, event type={getEventType()}" +
                     $", timestamp={getTimestamp()}, featureresult={featureResult.info()}] ";
 
--- ParcelPro/Assets/MetaCore/Scripts/Model/FeatureDetection/Feature.cs
+++ ParcelPro/Assets/MetaCore/Scripts/Model/FeatureDetection/Feature.cs
@@ -1,8 +1,8 @@
+// 필요한 네임스페이스를 사용합니다.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
-
 
 namespace Pnc
 {
@@ -10,46 +10,43 @@ namespace Pnc
     {
         namespace FeatureDetection
         {
+            // [System.Serializable] 어트리뷰트를 사용하여 클래스를 직렬화 가능하게 만듭니다.
             [System.Serializable]
-            public class Feature
+            public class Feature //[클래스 설명]특징의 파일 이름, 상태, 위치, 비율 정보를 저장하고 직렬화/역직렬화하는 클래스.
             {
+                // 파일 이름을 저장하는 속성을 정의합니다.
                 public string filename { get; set; }
+
+                // 상태를 저장하는 속성을 정의합니다.
                 public int status { get; set; }
 
+                // 위치를 저장하는 List를 정의하며 초기 크기는 8로 설정됩니다.
                 public List<float> position = new List<float>(8);
 
+                // 위치 비율을 저장하는 List를 정의하며 초기 크기는 8로 설정됩니다.
                 public List<float> positionratio = new List<float>(8);
 
+                // 영역 비율을 저장하는 속성을 정의합니다.
                 public float arearatio;
 
-                //public string rectpoint { get; set; }
-
-                public List<float> getPos()
-                {
-                    return position;
-                }
-
-                public List<float> getPosratio()
-                {
-                    return positionratio;
-                }
-
+                // JSON 문자열을 Feature 객체로 역직렬화하는 정적 메서드를 정의합니다.
                 public static Feature deserialize(string jsonString)
                 {
                     Feature feature = JsonConvert.DeserializeObject<Feature>(jsonString);
                     return feature;
                 }
 
+                // Feature 객체를 JSON 문자열로 직렬화하는 정적 메서드를 정의합니다.
                 public static string serialize(Feature jsonObject)
                 {
                     return JsonConvert.SerializeObject(jsonObject);
                 }
 
-
+                // 객체 정보를 문자열로 반환하는 메서드를 정의합니다.
                 public string info()
                 {
                     string buff = string.Empty;
-                    //buff = $"[id={id}, isDetect={status}, targetName={name}";
+                    // 이 메서드는 정보를 구성하지 않고 빈 문자열을 반환하고 있습니다.
                     return buff;
                 }
             }
