9e97de9731a1c7be1a25cb75d6e0d9056640e96a
LEE SE YEON <121601875+SEYEONPORTFOLIO@users.noreply.github.com>
2023-10-27 19:46:21 +0900

    NEW 카메라 띄우기
    스크립트 새로 생성 및 씬 생성

---
+++ ParcelPro/Assets/ParcelPro/Script/_ARCamera.cs
@@ -0,0 +1,91 @@
+using System;
+using UnityEngine;
+using UnityEngine.Android;
+using ZXing;
+
+public class _ARCamera : MonoBehaviour
+{
+    WebCamTexture camTexture;
+    public Renderer cameraRenderer;  // 웹캠 영상을 표시할 Renderer
+    public Vector3 rotationOffset = new Vector3(0, 0, 180);
+    public AudioSource sound;
+
+    private void Start()
+    {
+        CameraOn();
+    }
+
+    private void Update()
+    {
+        if (camTexture != null && camTexture.didUpdateThisFrame)
+        {
+            cameraRenderer.transform.rotation = Quaternion.Euler(rotationOffset);
+
+            // Update the camera texture
+            cameraRenderer.material.mainTexture = camTexture;
+            // Add your QR code detection logic here
+            try
+            {
+                IBarcodeReader barcodeReader = new BarcodeReader();
+
+                //웹캠 텍스처에서 QR코드 인식
+                var result = barcodeReader.Decode(camTexture.GetPixels32(), camTexture.width, camTexture.height);
+                if (result != null)
+                {
+                    Debug.Log("QR 코드: " + result.Text);
+                    sound.Play();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogWarning(ex.Message);
+            }
+        
+        }
+    }
+
+    public void CameraOn()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+
+        if (WebCamTexture.devices.Length == 0)
+        {
+            Debug.Log("No camera!");
+            return;
+        }
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        int selectedCameraIndex = -1;
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                selectedCameraIndex = i;
+                break;
+            }
+        }
+
+        if (selectedCameraIndex >= 0)
+        {
+            camTexture = new WebCamTexture(devices[selectedCameraIndex].name);
+            camTexture.requestedFPS = 30;
+            cameraRenderer.material.mainTexture = camTexture;
+            camTexture.Play();
+        }
+    }
+
+    public void CameraOff()
+    {
+        if (camTexture != null)
+        {
+            camTexture.Stop();
+            WebCamTexture.Destroy(camTexture);
+            camTexture = null;
+        }
+    }
+}
