d84135a6c0ef51fe6641fb411273e45367503593
LEE SE YEON <121601875+SEYEONPORTFOLIO@users.noreply.github.com>
2023-11-01 21:06:49 +0900

    QR Scene 완성

--- ParcelPro/Assets/ParcelPro/Script/_ARCamera.cs
+++ ParcelPro/Assets/ParcelPro/Script/_ARCamera.cs
@@ -11,13 +11,13 @@ public class _ARCamera : MonoBehaviour
     public Renderer cameraRenderer;  // 웹캠 영상을 표시할 Renderer
     public Vector3 rotationOffset = new Vector3(0, 0, 180);
     public AudioSource sound;
-    public GameObject canvasObject; // Unity Hierarchy에서 Canvas 오브젝트를 연결합니다.
-    public Text displayText;
+   // public GameObject canvasObject; // Unity Hierarchy에서 Canvas 오브젝트를 연결합니다.
+   // public Text displayText;
 
     private void Start()
     {
         CameraOn();
-        displayText.text = "QR Reader";
+       // displayText.text = "QR Reader";
     }
 
     private void Update()
@@ -49,8 +49,8 @@ public class _ARCamera : MonoBehaviour
                 }
 
                 // Canvas를 HMD 위치에 고정
-                canvasObject.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.0f; // 텍스트를 HMD에서 약간 떨어진 위치에 표시
-                canvasObject.transform.LookAt(Camera.main.transform); // 텍스트가 항상 HMD를 향하도록 설정
+              //  canvasObject.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.0f; // 텍스트를 HMD에서 약간 떨어진 위치에 표시
+               // canvasObject.transform.LookAt(Camera.main.transform); // 텍스트가 항상 HMD를 향하도록 설정
             }
             catch (Exception ex)
             {
